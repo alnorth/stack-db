7c10203538945ae766786b6138a9c9c0edf9e5d4
Alasdair North <alnorth@gmail.com>
2012-07-19 19:55:36 +0100

    Import answers

--- stack-db.py
+++ stack-db.py
@@ -20,17 +20,31 @@ def import_question(questions, id, title, body, tags, last_activity_date, score,
         "last_activity_date": last_activity_date,
         "score": score,
         "answer_count": answer_count,
-        "accepted_answer_id": accepted_answer_id
+        "accepted_answer_id": accepted_answer_id,
+        "answers": []
     }
     questions.insert(question)
-    print id
+
+def import_answer(questions, question_id, answer_id, body, last_activity_date, score):
+    answer = {
+        "answer_id": answer_id,
+        "body": body,
+        "last_activity_date": last_activity_date,
+        "score": score
+    }
+    question = questions.find_one({"question_id": question_id})
+    if question:
+        question["answers"].append(answer)
+        questions.update({"question_id": question_id}, question)
+    else:
+        print "Missing question %s" % question_id
 
 class SOProcessor(handler.ContentHandler):
 
     def startElement(self, name, attrs):
         if name == "row":
+            global questions
             if attrs["PostTypeId"] == "1":
-                global questions
                 import_question(
                     questions,
                     int(attrs["Id"]),
@@ -42,6 +56,17 @@ class SOProcessor(handler.ContentHandler):
                     int(attrs["AnswerCount"]) if "AnswerCount" in attrs else 0,
                     int(attrs["AcceptedAnswerId"]) if "AcceptedAnswerId" in attrs else 0
                 )
+            if attrs["PostTypeId"] == "2":
+                import_answer(
+                    questions,
+                    int(attrs["ParentId"]),
+                    int(attrs["Id"]),
+                    attrs["Body"],
+                    dateutil.parser.parse(attrs["LastActivityDate"]),
+                    int(attrs["Score"])
+                )
+            if int(attrs["Id"]) % 1000 == 0:
+                print attrs["Id"]
 
 parser = make_parser()
 parser.setContentHandler(SOProcessor())
