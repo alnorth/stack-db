04d8f6559169ff0be45810f1b2e95bfba12b124b
Alasdair North <alnorth@gmail.com>
2012-07-20 23:54:32 +0100

    Let's try again, this time with Map/Reduce.

--- stack-db.py
+++ stack-db.py
@@ -1,3 +1,4 @@
+from bson.code import Code
 import dateutil.parser
 from pymongo import Connection
 import sys
@@ -7,64 +8,70 @@ from xml.sax import make_parser, handler
 # Set up the database connection
 connection = Connection()
 db = connection.stackdb
-questions = db.questions
 
-questions.ensure_index("question_id", unique=True)
-questions.ensure_index("answers.answer_id", unique=True, sparse=True)
+if len(sys.argv) == 2:
+    # Set up a temporary collection for insertion
+    tmp_posts = db.tmp_posts
+    # Make sure it's empty
+    tmp_posts.remove()
 
-# For now we clear the collection before we start
-questions.remove()
+    tmp_posts.ensure_index("question_id", unique=True)
 
-class QuestionProcessor(handler.ContentHandler):
-    def startElement(self, name, attrs):
-        if name == "row":
-            if attrs["PostTypeId"] == "1":
-                question = {
-                    "question_id": int(attrs["Id"]),
-                    "title": attrs["Title"],
-                    "body": attrs["Body"],
-                    "tags": attrs["Tags"].lstrip("<").rstrip(">").split("><"),
-                    "last_activity_date": dateutil.parser.parse(attrs["LastActivityDate"]),
-                    "score": int(attrs["Score"]),
-                    "accepted_answer_id": int(attrs["AcceptedAnswerId"]) if "AcceptedAnswerId" in attrs else 0,
-                    "answers": []
-                }
+    class SOProcessor(handler.ContentHandler):
+        def startElement(self, name, attrs):
+            if name == "row":
+                if attrs["PostTypeId"] == "1":
+                    question = {
+                        "question_id": int(attrs["Id"]),
+                        "title": attrs["Title"],
+                        "body": attrs["Body"],
+                        "tags": attrs["Tags"].lstrip("<").rstrip(">").split("><"),
+                        "last_activity_date": dateutil.parser.parse(attrs["LastActivityDate"]),
+                        "score": int(attrs["Score"]),
+                        "accepted_answer_id": int(attrs["AcceptedAnswerId"]) if "AcceptedAnswerId" in attrs else 0,
+                        "answers": []
+                    }
+                    tmp_posts.insert(question)
 
-                # We assume that the question doesn't exist yet
-                questions.insert(question)
+                if attrs["PostTypeId"] == "2":
+                    answer = {
+                        "answer_id": int(attrs["Id"]),
+                        "body": attrs["Body"],
+                        "last_activity_date": dateutil.parser.parse(attrs["LastActivityDate"]),
+                        "score": int(attrs["Score"])
+                    }
+                    question = {
+                        "question_id": int(attrs["ParentId"]),
+                        "answers": [answer]
+                    }
+                    tmp_posts.insert(question)
 
-            if int(attrs["Id"]) % 5000 == 0:
-                print attrs["Id"]
+                if int(attrs["Id"]) % 5000 == 0:
+                    print attrs["Id"]
 
-class AnswerProcessor(handler.ContentHandler):
-    def startElement(self, name, attrs):
-        if name == "row":
-            if attrs["PostTypeId"] == "2":
-                answer = {
-                    "answer_id": int(attrs["Id"]),
-                    "body": attrs["Body"],
-                    "last_activity_date": dateutil.parser.parse(attrs["LastActivityDate"]),
-                    "score": int(attrs["Score"])
-                }
+    # We insert everything to start with, then use Map/Reduce to bring answers and questions together
 
-                # We assume that the question exists, but not the answer
-                result = questions.update({"question_id": int(attrs["ParentId"])}, { "$push": { "answers":  answer}}, safe=True)
-                if result["n"] != 1:
-                    print "Answer found for non-existent question, q=%s, a=%s" % (attrs["ParentId"], attrs["Id"])
+    print "Importing posts ..."
+    qparser = make_parser()
+    qparser.setContentHandler(SOProcessor())
+    qparser.parse(open(sys.argv[1]))
 
-            if int(attrs["Id"]) % 5000 == 0:
-                print attrs["Id"]
+    print "Running Map/Reduce ..."
+    map = Code("function () { emit(this.question_id, this); }")
+    reduce = Code("function (question_id, questions) {"
+                    "   var result = {};"
+                    "   questions.forEach(function(q) {"
+                    "       result.title = result.title || q.title;"
+                    "       result.body = result.body || q.body;"
+                    "       result.tags = result.tags || q.tags;"
+                    "       result.last_activity_date = result.last_activity_date || q.last_activity_date;"
+                    "       result.score = result.score || q.score;"
+                    "       result.accepted_answer_id = result.accepted_answer_id || q.accepted_answer_id;"
+                    "       result.answers = result.answers.concat(q.answers);"
+                    "   });"
+                    "   return result;"
+                    "}")
+    questions = db.tmp_posts.map_reduce(map, reduce, "questions")
+    print "Questions - %s" % questions.count()
 
-# In some of the data dumps answers appear before the questions they're answering.
-# We deal with this by running over the posts.xml twice. We import questions the
-# first time, and then answers.
-
-print "Importing questions ..."
-qparser = make_parser()
-qparser.setContentHandler(QuestionProcessor())
-qparser.parse(open(sys.argv[1]))
-
-print "Importing answers ..."
-aparser = make_parser()
-aparser.setContentHandler(AnswerProcessor())
-aparser.parse(open(sys.argv[1]))
+    db.tmp_posts.remove()
