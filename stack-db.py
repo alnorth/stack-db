5d292ee8a81eeeda77dfbedb7960d9e159fc0ef1
Alasdair North <alnorth@gmail.com>
2012-07-22 12:55:32 +0100

    Print progress when importing questions/answers.

--- stack-db.py
+++ stack-db.py
@@ -1,5 +1,7 @@
 from bson.code import Code
 import dateutil.parser
+import math
+import os
 from pymongo import Connection
 import sys
 import time
@@ -13,11 +15,16 @@ if len(sys.argv) == 2:
     # Set up a temporary collection for insertion
     tmp_posts = db.tmp_posts
     # Make sure it's empty
-    tmp_posts.remove()
+    tmp_posts.drop()
 
     tmp_posts.ensure_index("question_id")
 
     class SOProcessor(handler.ContentHandler):
+
+        def __init__(self, pf):
+            handler.ContentHandler.__init__(self)
+            self.pf = pf
+
         def startElement(self, name, attrs):
             if name == "row":
                 if attrs["PostTypeId"] == "1":
@@ -47,14 +54,40 @@ if len(sys.argv) == 2:
                     tmp_posts.insert(question)
 
                 if int(attrs["Id"]) % 5000 == 0:
-                    print attrs["Id"]
+                    self.pf.print_progress()
+
+    class PercentageFile(object):
+
+        def __init__(self, filename):
+            self.size = os.stat(filename)[6]
+            self.delivered = 0
+            self.f = file(filename)
+
+        def read(self, size=None):
+            if size is None:
+                self.delivered = self.size
+                return self.f.read()
+            data = self.f.read(size)
+            self.delivered += len(data)
+            return data
+
+        @property
+        def percentage(self):
+            return float(self.delivered) / self.size * 100.0
+
+        def print_progress(self):
+            hashes = int(math.floor(self.percentage)/2)
+            text = "\r[{0}{1}] {2}%".format("#" * hashes, " " * (50 - hashes), round(self.percentage, 1))
+            sys.stdout.write(text)
+            sys.stdout.flush()
 
     # We insert everything to start with, then use Map/Reduce to bring answers and questions together
 
     print "Importing posts ..."
     qparser = make_parser()
-    qparser.setContentHandler(SOProcessor())
-    qparser.parse(open(sys.argv[1]))
+    pf = PercentageFile(sys.argv[1])
+    qparser.setContentHandler(SOProcessor(pf))
+    qparser.parse(pf)
 
     print "Running Map/Reduce ..."
     map = Code("function () { emit(this.question_id, this); }")
